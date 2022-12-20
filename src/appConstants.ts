b3f86db07899917b607842d56e7a124456bbe523
fetz <emmsel@on.sinch.com>
2022-12-20 15:32:11 +0100

    constants for app config added

---
+++ src/appConstants.ts
@@ -0,0 +1,16 @@
+const FETCH_LIMIT = 15;
+const BASE_URL = 'https://chatty.doodle-test.com';
+const ENDPOINTS = '/api/chatty/v1.0/';
+
+const DEFAULT_AUTHOR = 'Anonymous';
+const API_TOKEN = 'FGvviHC9KSai';
+const BEGIN_SINCE = 1637869064412;
+
+export {
+  FETCH_LIMIT,
+  DEFAULT_AUTHOR,
+  API_TOKEN,
+  BASE_URL,
+  ENDPOINTS,
+  BEGIN_SINCE,
+};
